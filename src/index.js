17df2b1342652f2ed784b7c352bc7171b7ac50f3
arwinada <arwin.chan@ada.support>
2025-04-21 15:21:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import myFirstApp from './my-first-app';
 import myFirstAdvancedKviewApp from './my-first-advanced-kview-app';
 import myFirstWidgetApp from './my-first-widget-app';
 import myFirstOutboundWebhookApp from './my-first-outbound-webhook-app';
-import tutorialApp from './giphy-tutorial_]-app';
+import tutorialApp from './giphytutorialapp';
 
 const apps = [
     myFirstApp,
