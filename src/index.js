8da376fc4e81fffe19b3c69a4cc1bc81d270ea33
arwinada <arwin.chan@ada.support>
2025-04-21 13:02:17 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import myFirstApp from './my-first-app';
 import myFirstAdvancedKviewApp from './my-first-advanced-kview-app';
 import myFirstWidgetApp from './my-first-widget-app';
 import myFirstOutboundWebhookApp from './my-first-outbound-webhook-app';
+import tutorialApp from './giphy_tutorial_par-ada-delilah';
 
 const apps = [
     myFirstApp,
     myFirstWidgetApp,
     myFirstAdvancedKviewApp,
     myFirstOutboundWebhookApp,
+    tutorialApp
 ];
 
 export { apps as default };
